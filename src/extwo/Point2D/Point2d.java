15e29212db5a50ef610cd25036d92b7c9c56253d
anhdk581998 <anh0941750335@gmail.com>
2023-09-15 17:10:28 +0700

    gui em linh

---
+++ src/extwo/Point2D/Point2d.java
@@ -0,0 +1,33 @@
+package extwo.Point2D;
+class Point2d {
+    private float x;
+    private float y;
+
+    public Point2d(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    public void setXY(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+    public float[] getXY(){
+        return foat[2]  = float x, float y}
+    }
+}
